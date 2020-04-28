88669f5da52b064fc7d00c9029588b617b6e4ec7
Yukiko <yukikogimi@gmail.com>
2020-04-29 01:17:08 +1000

    stripe integrated

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -20,7 +20,7 @@ export default class Modal extends Component {
               <p>$ {this.props.price}</p>
               <img src={this.props.img} className="img-fluid" style={{width:"10em", height:"10em"}}/>
               
-              <button onClick={this.props.closeModal} style={{display:'block', margin:"0 auto", color:'white', backgroundColor:'orange', borderRadius:"2rem", marginTop:"px"}}>Back to Store</button>
+              <button onClick={this.props.closeModal} style={{display:'block', margin:"0 auto", color:'white', backgroundColor:'orange', borderRadius:"2rem", marginTop:"10px"}}>Back to Store</button>
              
             </div>
           </div>
