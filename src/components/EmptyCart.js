046f21ccceffacd5284a77079bcac31e2e8f8d35
Yukiko <yukikogimi@gmail.com>
2020-04-30 00:15:59 +1000

    fized but start over with front page

--- src/components/EmptyCart.js
+++ src/components/EmptyCart.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Empty from "./css/Empty.css"
 
 export default function EmptyCart() {
   return (
     <div className="container mt-5">
       <div className="row">
-        <div className="col-10 mx-auto text-center mb-8">
-          <h2>The cart is empty</h2>
+        <div className="col-10 mx-auto text-center mb-6">
+          <h2>The cart is currently empty</h2>
           <i class="fas fa-shopping-cart fa-5x"></i>
           <br></br>
           <Link to="/pokemons" styled={{ textDecoration: "none" }}>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,11 @@
 import React from "react";
 import "./css/Home.css";
-import MultiPlayer from "./MultiPlayer";
-import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div>
-      <h1>this is top page Hello World</h1>
-      <p>I want to go back here</p>
+    <form></form>
+      
     </div>
   );
 }
-
-////
-//
-//tala lan lan lan
-
-///
-// https://vgmdownloads.com/soundtracks/pokemon-gameboy-sound-collection/xwvbnmtw/110-pokemon%20center.mp3
-//pokemon center
-// </div>
-//           <div class="stage">
-//           <div class="poke bounce"></div>
