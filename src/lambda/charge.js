d243e107451620aac8f353ea275a11549e2d6c32
Yukiko <yukikogimi@gmail.com>
2020-04-30 17:43:33 +1000

    description of shop changed

--- src/lambda/charge.js
+++ src/lambda/charge.js
@@ -21,7 +21,7 @@ exports.handler = (event, context, callback) => {
     .create({
       amount: parseInt(data.amount),
       currency: "usd",
-      description: "Dreamcast game shop",
+      description: "poke shop",
       source: data.token,
     })
     .then(({ status }) => {
