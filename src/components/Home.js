9cd6e820266a7068ab1eca39664473ad47d56d65
gimikon <37206572+gimikon@users.noreply.github.com>
2020-07-21 13:26:38 +1000

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -62,7 +62,7 @@ export default class Home extends Component {
               </div>
             </div>
 
-            <Footer />
+           
           </React.Fragment>
         )}
       </PokemonConsumer>
